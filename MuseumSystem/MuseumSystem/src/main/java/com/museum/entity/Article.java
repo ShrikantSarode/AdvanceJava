2230a27b9347286de684f4b9660e71aa877b0435
Shrikant <srikantsarode786@gmail.com>
2025-01-02 09:03:37 +0530

    spring boot

---
+++ MuseumSystem/MuseumSystem/src/main/java/com/museum/entity/Article.java
@@ -0,0 +1,9 @@
+package com.museum.entity;
+
+public class Article {
+
+	private Integer id;
+	private String name;
+	private Category category;
+	
+}
---
+++ Practice/src/typecasting/TypeCasting.java
@@ -0,0 +1,14 @@
+package typecasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		double b = a;
+		
+		double c=20;
+		int d= (int) c;
+		
+	}
+}
